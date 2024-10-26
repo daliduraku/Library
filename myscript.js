715e80a204f682060b750c63f8f5fec902a34a41
daliduraku <mesherbetmolla@gmail.com>
2024-10-26 12:15:55 -0400

    created the function addBookToLibrary, where we create an instance of the book class and add it to the array of book objects

--- myscript.js
+++ myscript.js
@@ -7,17 +7,26 @@ class Book {
         this.title = title; //string
         this.pages = pages; //num
         this.read = read; // string
+        
     }
+}
 
+function addBookToLibrary(author, title, pages, read) {
+     // creating a new object
+     let book = new Book(author, title, pages, read);
 
+     // pushing the new book in the array of objects
+     myLibrary.push(book);
 }
 
 const button = document.getElementById("showFormBtn");
 const form = document.getElementById("myform");
 
 button.addEventListener("click", function(){
+    // getting the default value
     const formDisplay = window.getComputedStyle(form).display;
 
+    // if the button is clicked we show the form
     if (formDisplay === "none"){
         form.style.display = "block";
     }else {
@@ -25,9 +34,21 @@ button.addEventListener("click", function(){
     }
 });
 
-const submitButton = document.getElementById("submitBtn")
-
-submitButton.addEventListener("click", function(event){
+document.querySelector('form').addEventListener('submit', function(event){
     event.preventDefault();
+
+    // getting data from the form
+    const formData = new FormData(event.target);
+
+    // creating variables from the data
+
+    const author = formData.get('author');
+    const title = formData.get('title');
+    const pages = formData.get('pages'); 
+    const read = formData.has('read') ? "The book was read" : "The book was not read";
     
+    // calling function to create a new book and add to the library 
+    addBookToLibrary(author, title, pages, readString);
+
 })
+
