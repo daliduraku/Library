f9a38baaec5732f4eaf173d3d01f65f0c30dc9d7
daliduraku <mesherbetmolla@gmail.com>
2024-10-25 23:04:55 -0400

    first commit

---
+++ myscript.js
@@ -0,0 +1,21 @@
+
+const  myLibrary = [];
+
+class Book {
+    constructor(author, title, pages, read) {
+        this.author = author; //string
+        this.title = title; //string
+        this.pages = pages; //num
+        this.read = read; // string
+    }
+
+
+}
+
+const button = document.getElementById("showFormBtn");
+const form = document.getElementById("myForm");
+
+
+button.addEventListener("click", function(){
+    
+})
